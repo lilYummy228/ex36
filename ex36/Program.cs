e0a4c96db7f419473c8e17b35e70c112f948a080
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-27 11:45:05 +0500

    Completed

--- ex36/Program.cs
+++ ex36/Program.cs
@@ -10,7 +10,29 @@ namespace ex36
     {
         static void Main(string[] args)
         {
+            string[] firstNumbersRange = { "1", "2", "1" };
+            string[] secondNumbersRange = { "3", "2" };
 
+            List<string> attachment = new List<string>();
+
+            AddNonRepeatedNumber(attachment, firstNumbersRange.Length, firstNumbersRange);
+            AddNonRepeatedNumber(attachment, secondNumbersRange.Length, secondNumbersRange);
+
+            foreach (var number in attachment)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static void AddNonRepeatedNumber(List<string> attachment, int numbersRangeLength, string[] numbersRange)
+        {
+            for (int i = 0; i < numbersRangeLength; i++)
+            {
+                if (attachment.Contains(numbersRange[i]) == false)
+                {
+                    attachment.Add(numbersRange[i]);
+                }
+            }
         }
     }
 }
